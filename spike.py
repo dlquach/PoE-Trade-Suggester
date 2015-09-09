92df082ae6461d9c6bb1e57b751605f338f6d6d5
dlquach <derekq.alt@gmail.com>
2015-09-08 19:47:55 -0500

    Added price sorting to query results

--- spike.py
+++ spike.py
@@ -1,5 +1,7 @@
 from trade_api.poe_trade_interface import POETradeInterface
 
 interface = POETradeInterface()
-search_args = { "type" : ["Bow"]}
-print interface.get_query_url(search_args)
+search_args = {"type" : ["Bow"], "pdps_min" : "300"}
+url = interface.get_query_url(search_args)
+print url
+interface.get_cheapest_query_results(url)
--- trade_api/poe_trade_interface.py
+++ trade_api/poe_trade_interface.py
@@ -7,8 +7,13 @@ Required features:
     - Get specific item information for display
 """
 
-from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
 import mechanize
+
+from BeautifulSoup import BeautifulSoup
+from tabulate import tabulate
+from natsort import natsorted
 
 # Sanitize bad HTML so mechanize doens't die
 # Code found on StackOverflow 
@@ -36,6 +41,9 @@ class POETradeInterface():
 
         return br
 
+    def __init__(self):
+        self._browser = self._init_browser()
+
     def get_query_url(self, search_args):
         """
         Queries poe.trade with the provided parameters and returns the corresponding
@@ -55,28 +63,126 @@ class POETradeInterface():
 
             Example output: "http://poe.trade/search/imakumakaugogo"
         """
-        browser = self._init_browser()
-        browser.open("http://poe.trade/")
+        self._browser.open("http://poe.trade/")
 
         # There are two forms, the second one is the search form
         # Both forms don't have names so we just know the 2nd one is the right one
-        browser.form = list(browser.forms())[1]
+        self._browser.form = list(self._browser.forms())[1]
 
         # Populate the forms with the stuff we want
         for form_name in search_args:
-            control = browser.form.find_control(form_name)
+            control = self._browser.form.find_control(form_name)
             control.value = search_args[form_name]
 
         # By default we want people are are online and accepting buyouts
-        buyout_control = browser.form.find_control("buyout")
-        online_control = browser.form.find_control("online")
+        buyout_control = self._browser.form.find_control("buyout")
+        online_control = self._browser.form.find_control("online")
         buyout_control.value = ["x"]
         online_control.value = ["x"]
         
-        search_response = browser.submit()
+        search_response = self._browser.submit()
         return search_response.geturl()
+    
+    def get_query_url_results(self, url):
+        """
+        Takes in a query and returns an array containing all of the item data.
 
+        TODO: Return a dictionary instead of this ugly array
+        """
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        page = response.read()
+        soup = BeautifulSoup(page)
 
+        headers = ["Item Name", "Seller IGN", "Sockets", "Price"]
+        names = []
+        sellers = []
+        sockets = []
+        prices = []
+
+        # itemframe2 indicates a rare, itemframe3 indicates a unique
+        name_search = soup.findAll("a", {"class" : ["title itemframe2", "title itemframe3"]})
+        for item in name_search:
+            raw_name = ''.join(item.findAll(text=True))
+            if "corrupted" in raw_name:
+                names.append(raw_name.replace("corrupted ", '') + '(C)')
+            else:
+                names.append(raw_name)
+
+        price_search = soup.findAll("tbody", {"class" : "item"})
+        for item in price_search:
+            prices.append(item["data-buyout"])
+
+        socket_search = soup.findAll("span", {"class" : "sockets-raw"})
+        for item in socket_search:
+            sockets.append(item.text)
+
+        seller_search = soup.findAll("span", {"class" : "requirements"})
+
+        for item in seller_search:
+            find_ign = re.search('IGN:\ ([A-Za-z0-9_]*)\ ', item.text)
+            if find_ign:
+                sellers.append(find_ign.group(1))
+
+        data = zip(names, sellers, sockets, prices)
+        return data
+
+    def _sort_by_price(self, data):
+        """
+        Sorts a given dataset by price (separating the currencies accordingly since we 
+        don't have accurate exchange rate data). That is, sort numerically but split up
+        currency types (alch < fusing < chaos < exalted).
+        
+        Args:
+            array: in the form of [(<name>, <IGN>, <links>, <price>)]
+        
+        Returns:
+            array: sorted version of the passed in array
+        """
+        # Separate the data by currency   
+        alch = []
+        fusing = []
+        chaos = []
+        exalted = []
+
+        # We know that the item at index 3 is the price in the format of "# <currency>"
+        for item in data:
+            if "alchemy" in item[3]:
+                alch.append(item)
+            elif "fusing" in item[3]:
+                fusing.append(item)
+            elif "chaos" in item[3]:
+                chaos.append(item)
+            elif "exalted" in item[3]:
+                exalted.append(item)
+
+        alch = natsorted(alch, key=lambda item: item[3])
+        fusing = natsorted(fusing, key=lambda item: item[3])
+        chaos = natsorted(chaos, key=lambda item: item[3])
+        exalted = natsorted(exalted, key=lambda item: item[3])
+
+        result = []
+        result.extend(alch)
+        result.extend(fusing)
+        result.extend(chaos)
+        result.extend(exalted)
+        return result
+    
+    def get_cheapest_query_results(self, url):
+        """
+        Takes in a query url, processes the results, and returns the top 5 cheapest items
+        that match our criteria.
+        
+        Args:
+            url (string): the url for the search we want to parse
+        
+        Returns:
+            dict: items, their prices, and their stats 
+        """
+        data = self.get_query_url_results(url)
+        data = self._sort_by_price(data)
+        headers = ["Item Name", "Seller IGN", "Sockets", "Price"]
+        print tabulate(data, headers, tablefmt="rst")
 
 
 
